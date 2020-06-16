9affbc59d73a4c8fa1a1c4698b92470d24e8caea
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-06-16 14:46:39 +0200

    Settings can force the language to use

--- src/AppLayout/Main/Main.js
+++ src/AppLayout/Main/Main.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
 
 import Nav from '../Nav';
 import Content from '../Content';
 
 import './main.scss';
 
-export const Main = ({ isNavBarVisible }) => (
-  <main className="main">
-    {isNavBarVisible && <Nav />}
-    <Content />
-  </main>
-);
+export const Main = ({
+  isNavBarVisible,
+  env: { language } = {},
+}) => {
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    if (!language) return;
+    i18n.changeLanguage(language);
+  }, [i18n, language]);
+
+  return (
+    <main className="main">
+      {isNavBarVisible && <Nav />}
+      <Content />
+    </main>
+  );
+};
+
+Main.propTypes = {
+  isNavBarVisible: PropTypes.bool,
+  env: PropTypes.shape({
+    language: PropTypes.string,
+  }),
+};
+
+Main.defaultProps = {
+  isNavBarVisible: false,
+  env: {
+    language: undefined,
+  },
+};
 
 export default Main;
