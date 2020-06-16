15f516d24c952cfeb4711ef2eb3fca9c5df422c1
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-06-16 15:02:19 +0200

    Use Helmet to change dynamically HEAD content tag

--- src/AppLayout/Main/Main.js
+++ src/AppLayout/Main/Main.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
+import Helmet from 'react-helmet';
 
 import Nav from '../Nav';
 import Content from '../Content';
@@ -9,9 +10,10 @@ import './main.scss';
 
 export const Main = ({
   isNavBarVisible,
-  env: { language } = {},
+  env: { favicon, language, title } = {},
 }) => {
   const { i18n } = useTranslation();
+
 
   useEffect(() => {
     if (!language) return;
@@ -20,6 +22,20 @@ export const Main = ({
 
   return (
     <main className="main">
+      <Helmet>
+        <html lang={i18n.language} prefix="og:http://ogp.me/ns#" />
+        {title && (
+          <title>{title}</title>
+        )}
+        {title && (
+          <meta property="og:title" content={title} />
+        )}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={global.location.href} />
+        {favicon && (
+          <link rel="shortcut icon" href={favicon} />
+        )}
+      </Helmet>
       {isNavBarVisible && <Nav />}
       <Content />
     </main>
@@ -29,14 +45,18 @@ export const Main = ({
 Main.propTypes = {
   isNavBarVisible: PropTypes.bool,
   env: PropTypes.shape({
+    favicon: PropTypes.string,
     language: PropTypes.string,
+    title: PropTypes.string,
   }),
 };
 
 Main.defaultProps = {
   isNavBarVisible: false,
   env: {
+    favicon: undefined,
     language: undefined,
+    title: undefined,
   },
 };
 
