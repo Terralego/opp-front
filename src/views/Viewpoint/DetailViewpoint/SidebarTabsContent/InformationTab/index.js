afdb951d7babdc19e71d0dfbd98e96501a9b826c
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2021-04-27 22:36:12 +0200

    Split themes in categories

--- src/views/Viewpoint/DetailViewpoint/SidebarTabsContent/InformationTab/index.js
+++ src/views/Viewpoint/DetailViewpoint/SidebarTabsContent/InformationTab/index.js
@@ -1,11 +1,17 @@
 import React, { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
+import Api from '@terralego/core/modules/Api';
+
+import { useQuery } from 'react-query';
 import IconBlock from '../../../../../components/IconBlock';
 import CollapsiblePanel from '../../../../../components/CollapsiblePanel';
 import DateDisplay from '../../../../../components/DateDisplay';
 
 import { connectViewpointProvider } from '../../../context';
+import { useSettings } from '../../../../../components/SettingsProvider';
 import Minimap from './Minimap';
+
+const pageSize = 100;
 
 export const InformationTab = ({
   viewpoint: {
@@ -16,14 +22,57 @@ export const InformationTab = ({
   },
 }) => {
   const { t } = useTranslation();
+  const {
+    modules: {
+      OPP: { theme_categories: themeCategories },
+    },
+  } = useSettings();
 
-  const getViewpointInfos = useCallback((streetName, cityName, siteName) => {
-    if (streetName || cityName) {
-      return [streetName, cityName].filter(Boolean).join(' - ');
-    }
-    return siteName || t('viewpoint.detail.information.no-info');
-  }, [t]);
+  const getViewpointInfos = useCallback(
+    (streetName, cityName, siteName) => {
+      if (streetName || cityName) {
+        return [streetName, cityName].filter(Boolean).join(' - ');
+      }
+      return siteName || t('viewpoint.detail.information.no-info');
+    },
+    [t],
+  );
 
+  const { data: allThemes, loaded } = useQuery('themes', async () => {
+    // First page to know total
+    const { results: firstPage, num_pages: pageCount } = await Api.request('themes/', {
+      querystring: { page_size: pageSize },
+    });
+
+    // Get all next pages
+    const all = await Promise.all(
+      Array.from({ length: pageCount - 1 }, async (_, index) => {
+        const { results: nextPage } = await Api.request('themes/', {
+          querystring: { page_size: pageSize, page: index + 2 },
+        });
+        return nextPage;
+      }),
+    );
+
+    // Flatten all results
+    return [firstPage, ...all].flat(1);
+  });
+
+  const themeMap = React.useMemo(
+    () =>
+      loaded
+        ? allThemes
+            .filter(({ label }) => themes.includes(label))
+            .reduce((acc, theme) => {
+              if (!acc[theme.category]) {
+                acc[theme.category] = [];
+              }
+              acc[theme.category].push(theme);
+              return acc;
+            }, {})
+        : {},
+    [allThemes, loaded, themes],
+  );
 
   return (
     <div className="panel-details">
@@ -61,15 +110,25 @@ export const InformationTab = ({
       </CollapsiblePanel>
       {themes && (
         <CollapsiblePanel title={t('viewPoint.detail.information.landscapes')} initialState={false}>
-          <IconBlock
-            title={t('viewPoint.detail.information.landscapesContent')}
-            icon="tag"
-            content={[...new Set(themes)].map(theme => (
-              <p key={theme} className="badge">
-                {theme}
-              </p>
-            ))}
-          />
+          {themeMap &&
+            themeCategories
+              .filter(({ id }) => Object.keys(themeMap).includes(id))
+              .map(({ id, name, color }) => {
+                if (themeMap[id]) {
+                  return (
+                    <IconBlock
+                      title={name}
+                      icon="tag"
+                      content={themeMap[id].map(({ label }) => (
+                        <p key={label} className="badge" style={{ backgroundColor: color }}>
+                          {label}
+                        </p>
+                      ))}
+                    />
+                  );
+                }
+                return null;
+              })}
         </CollapsiblePanel>
       )}
     </div>
