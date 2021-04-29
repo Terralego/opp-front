b2ac29bf7578a569dc5a027a001b50a14bce82a6
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2021-04-29 16:09:26 +0200

    Add password reset

--- src/AppLayout/Content/routes/index.js
+++ src/AppLayout/Content/routes/index.js
@@ -3,6 +3,7 @@ import { lazy } from 'react';
 const Map = lazy(() => import('../../../views/Map'));
 
 const Viewpoint = lazy(() => import('../../../views/Viewpoint'));
+const Profile = lazy(() => import('../../../AppLayout/Profile'));
 
 const routes = [
   {
@@ -13,6 +14,10 @@ const routes = [
   {
     Component: Viewpoint,
     path: '/viewpoint/:id/:imageView?',
+  },
+  {
+    Component: Profile,
+    path: '/create-account/:id/:token',
   },
 ];
 
