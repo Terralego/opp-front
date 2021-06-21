63e94c290056cbe0edb9aaaabfd6f63b86a73dd3
CFarcy <cedric.farcy@makina-corpus.com>
2021-06-21 14:25:36 +0200

    Remove highlight marker  from unmounted result component (#47)

--- src/views/Map/Results/ResultItem/ResultItem.js
+++ src/views/Map/Results/ResultItem/ResultItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as PropTypes from 'prop-types';
 
 import { Button } from '@blueprintjs/core';
@@ -14,20 +14,24 @@ const ResultItem = ({
   onMouseEnterResult,
   onMouseLeaveResult,
   picture: { list: picture },
-}) => (
-  <Link
-    to={`viewpoint/${id}`}
-    className="result"
-    onMouseEnter={() => onMouseEnterResult(id)}
-    onMouseLeave={() => onMouseLeaveResult(id)}
-    onFocus={() => onMouseEnterResult(id)}
-    onBlur={() => onMouseLeaveResult(id)}
-  >
-    <h3 className="result-title">{label}</h3>
-    <img className="result-picture" src={picture} alt={label} />
-    <Button className="result-action" text={t('map.results.access')} />
-  </Link>
-);
+}) => {
+  // remove highligh when component is unmounted
+  useEffect(() => () => onMouseLeaveResult(id), [onMouseLeaveResult, id]);
+  return (
+    <Link
+      to={`viewpoint/${id}`}
+      className="result"
+      onMouseEnter={() => onMouseEnterResult(id)}
+      onMouseLeave={() => onMouseLeaveResult(id)}
+      onFocus={() => onMouseEnterResult(id)}
+      onBlur={() => onMouseLeaveResult(id)}
+    >
+      <h3 className="result-title">{label}</h3>
+      <img className="result-picture" src={picture} alt={label} />
+      <Button className="result-action" text={t('map.results.access')} />
+    </Link>
+  );
+};
 
 ResultItem.propTypes = {
   id: PropTypes.number,
